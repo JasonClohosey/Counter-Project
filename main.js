81e6bf979bf215c47c2111fc49289d21163cb3c0
Jason Clohosey <clohoseyjason@gmail.com>
2020-05-21 17:17:03 -0400

    Finished JavaScript / fully functional. Finished styling in CSS

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-alert ("Hello")
+var number = 0
+
+function counter() {
+document.getElementById("plusBtn").addEventListener("click", function() {
+        number++;
+        document.getElementById("number").innerHTML = number;
+        if (number == 0) {
+            document.getElementById("number").style.color = "white";
+        }else if (number > 0) {
+        document.getElementById("number").style.color = "green";
+        }else {
+            document.getElementById("number").style.color = "red";
+        };
+        console.log("clicked button ", number);
+    });
+
+    document.getElementById("minusBtn").addEventListener("click", function() {
+        number--;
+        document.getElementById("number").innerHTML = number;
+        if (number == 0) {
+            document.getElementById("number").style.color = "white";
+        }else if (number > 0) {
+        document.getElementById("number").style.color = "green";
+        }else {
+            document.getElementById("number").style.color = "red";
+        };
+        console.log("clicked button ", number);
+    });
+    
+};
+
+counter()
